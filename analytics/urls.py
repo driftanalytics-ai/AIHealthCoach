56c02f7f4ec7a3ca62d1b05943547530cd92612a
Utkarsh Parkhi <uparkhi@ph.iitr.ac.in>
2024-11-11 14:39:38 -0500

    add metrics query view

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,14 +1,13 @@
+from analytics.views import QueryViewSet, graph_view, metric_info
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-
-from analytics.views import QueryViewSet, general_info, graph_view
 
 router = SimpleRouter()
 router.register("query", QueryViewSet)
 
 urlpatterns = [
     path("graph/", graph_view),
-    path("general/", general_info),
+    path("metrics/", metric_info),
     path("", include(router.urls)),
 ]
 print(router.urls)
